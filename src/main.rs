f1ec0457d2830dca5bba5cf19b662524bdede3bb
Olafur Bogason <olafur@genkiinstruments.com>
2019-09-22 12:30:10 +0000

    Fix clippy and rustfmt errors.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::env;
-use std::io::Read;
 use std::fs::File;
+use std::io::Read;
 
 use piston_window::*;
 
 mod cpu;
 
-
 fn main() {
-    let file_name = env::args().nth(1).expect("Must give game name as first file");
+    let file_name = env::args()
+        .nth(1)
+        .expect("Must give game name as first file");
     let mut file = File::open(file_name).expect("There was an issue opening the file");
     let mut buffer = Vec::new();
     let _bytes_read = file.read_to_end(&mut buffer);
@@ -16,23 +17,35 @@ fn main() {
     let mut cpu = cpu::Cpu::new(buffer.as_slice());
 
     const GUI_SCALE: f64 = 10.0;
-    let window_dimensions = [cpu::VIDEO_WIDTH * GUI_SCALE as u32, cpu::VIDEO_HEIGHT * GUI_SCALE as u32];
+    let window_dimensions = [
+        cpu::VIDEO_WIDTH * GUI_SCALE as u32,
+        cpu::VIDEO_HEIGHT * GUI_SCALE as u32,
+    ];
     let mut window: PistonWindow = WindowSettings::new("Chip-8 Interpreter", window_dimensions)
         .exit_on_esc(true)
         .build()
         .expect("Failed to create a piston window");
 
     while let Some(e) = window.next() {
-        if let Some(_) = e.render_args() {
+        if e.render_args().is_some() {
             window.draw_2d(&e, |context, graphics, _| {
                 piston_window::clear(color::BLACK, graphics);
 
                 for (i, row) in cpu.video.chunks(cpu::VIDEO_WIDTH as usize).enumerate() {
                     for (j, &val) in row.iter().enumerate() {
                         if val > 0 {
-                            let d = [j as f64 * GUI_SCALE, i as f64 * GUI_SCALE, GUI_SCALE, GUI_SCALE];
-                            Rectangle::new(color::WHITE)
-                                .draw(d, &context.draw_state, context.transform, graphics);
+                            let d = [
+                                j as f64 * GUI_SCALE,
+                                i as f64 * GUI_SCALE,
+                                GUI_SCALE,
+                                GUI_SCALE,
+                            ];
+                            Rectangle::new(color::WHITE).draw(
+                                d,
+                                &context.draw_state,
+                                context.transform,
+                                graphics,
+                            );
                         }
                     }
                 }
